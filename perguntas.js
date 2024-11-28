4178f8bfc0b76b4308a703b4b9e6e80d6523a541
Duda2312 <mariaeduardaaislva@gmail.com>
2024-11-28 15:45:04 -0300

    adicionando arquivos

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'O que é uma fração?',
+    'Fração é uma representação de uma parte do todo, expressa como a razão entre dois números: o numerador (parte) e o denominador (todo)'
+)
+
+criaCartao(
+    'História',
+    'Quem proclamou a independência do Brasil?',
+    'Dom Pedro I proclamou a independência do Brasil em 7 de setembro de 1822.'
+)
+
+criaCartao(
+    'Sociologia',
+    'O que significa o termo "socialização"?',
+    'Socialização é o processo pelo qual os indivíduos aprendem e internalizam as normas, valores e comportamentos de uma sociedade.'
+)
+
+criaCartao(
+    'Português',
+    'O que significa sujeito em uma oração?',
+    'O sujeito é o termo da oração que indica quem pratica ou sofre a ação expressa pelo verbo ou de quem se fala.'
+)
